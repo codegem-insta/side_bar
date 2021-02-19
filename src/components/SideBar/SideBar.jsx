ab4210af6fd10f99cd7f09b733a65985132c10de
Codegem <codegemorg@gmail.com>
2021-02-19 07:54:49 -0500

    Added close icon

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Hamburger } from "../Hamburger/Hamburger";
+import close from './close.svg';
 
 import style from "./SideBar.module.css";
 
@@ -12,8 +13,12 @@ function SideBar() {
     <>
       <Hamburger onClick={toggleSidebar} />
 
-      <div className={isOpen ? `${style.SideBar} ${style.Active}` : style.SideBar}>
-        <div className={style.Close} onClick={toggleSidebar}>Close</div>
+      <div
+        className={isOpen ? `${style.SideBar} ${style.Active}` : style.SideBar}
+      >
+        <div className={style.Close} onClick={toggleSidebar}>
+          <img src={close} alt="close" />
+        </div>
         <div>Home</div>
         <div>Messages</div>
         <div>Log out</div>
